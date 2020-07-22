4c4e11af09f14005e2dfd9703d2bdf2d47913ff9
chang Liu <knightt0258@gmail.com>
2020-07-23 01:44:28 +1200

    update text and logo

---
+++ my-app/src/App.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="https://openmoji.org/data/color/svg/1F532.svg" className="App-logo" alt="logo" />
+        <p>
+          MSA 2020
+        </p>
+        <a
+          className="App-link"
+          href="https://aka.ms/nzmsawebsite"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          MSA website
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
